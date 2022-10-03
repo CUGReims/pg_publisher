7baa24822f3dca52775bc90ae823337276f0203f
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2022-10-03 15:28:28 +0200

    Add publish

--- Makefile
+++ Makefile
@@ -6,5 +6,12 @@ build: docker-build-publisher
 docker-build-publisher:
 	docker build -t camptocamp/reims_publisher:latest reims_publisher
 
+up:
+	docker-compose up -d
+
 tests:
-	docker-compose run --rm --user `id -u`:`id -g` tester
+	docker-compose exec --user `id -u`:`id -g` tester pytest /src/tests -vv
+
+clean:
+	docker-compose down -v -t1
+	docker rmi camptocamp/reims_publisher:latest || true
---
+++ reims_publisher/reims_publisher/core/publish.py
@@ -0,0 +1,36 @@
+import io
+import multiprocessing
+import subprocess
+import tempfile
+from typing import List, Optional
+
+
+def publish(
+    src_conn_string: str,
+    dst_conn_string: str,
+    schemas: Optional[List[str]] = None,
+    tables: Optional[List[str]] = None,
+    views: Optional[List[str]] = None,
+    force: Optional[bool] = False,
+):
+    receiver = subprocess.Popen(
+        ["psql", dst_conn_string],
+        stdin=subprocess.PIPE,
+    )
+
+    for schema in schemas:
+        receiver.stdin.write("DROP SCHEMA IF EXISTS {} CASCADE;".format(schema).encode("utf8"))
+
+    sender = subprocess.Popen(
+        [
+            "pg_dump"
+        ] + [
+            arg for schema in schemas for arg in ["-n", schema]
+        ] + [
+            src_conn_string
+        ],
+        stdout=receiver.stdin,
+        stderr=subprocess.PIPE,
+    )
+
+    receiver.communicate()
