b22e4b62a50d534f4fb0bcd6346bf5bcb68bf4b0
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2022-10-03 13:47:08 +0200

    Dockerize

--- reims_publisher/setup.py
+++ reims_publisher/setup.py
@@ -10,9 +10,10 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read().splitlines()
 
-test_requirements = ['pytest>=3', ]
+test_requirements = ['', ]
 
 setup(
     author="Camptocamp",
